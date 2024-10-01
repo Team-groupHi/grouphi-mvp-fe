1c27cee8a9fb6ccf8518bb58544e7243a1ca3fab
SoJuSo <asdfg7123@gmail.com>
2024-10-01 21:32:22 +0900

    🔧chore: sentry 기능 설치

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://fcb0a002d14b860c7dc97c506eb5ed23@o4508047439101952.ingest.us.sentry.io/4508047440216064',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
