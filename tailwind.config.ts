306c2ae3df67ab8ac8eccc071a86a9929a3fe724
Suyoung Shin <joyswim.dev@gmail.com>
2024-10-14 14:33:47 +0900

    🔧chore: tailwind.config.ts 기초 디자인 시스템 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,99 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primary: {
+        light: '#93EFDE',
+        DEFAULT: '#32E4D0',
+        dark: '#00CDB6',
+        container: '#000000',
+      },
+      secondary: {
+        light: '#E6A3FF',
+        DEFAULT: '#CB78EA',
+        dark: '#B355D5',
+        container: '#000000',
+      },
+      tertiary: {
+        DEFAULT: '#FFF0B6',
+        container: '#000000',
+      },
+      error: {
+        DEFAULT: '#E83A34',
+        container: '#000000',
+      },
+      text: {
+        DEFAULT: '#F0F0F0',
+        dark: '#B3B3B3',
+      },
+      gray: {
+        100: '#F2F2F7',
+        200: '#E5E5EA',
+        300: '#C7C7CC',
+        500: '#AEAEB2',
+        700: '#808080',
+        900: '#404040',
+      },
+      black: '#000000',
+      white: '#ffffff',
+    },
     extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+      backgroundImage: {
+        'gradient-purple':
+          'linear-gradient(#350D6D 0%, #352778 20%, #5D4DA6 85%)',
+        'gradient-primary': 'linear-gradient(120deg, #32E4D0 0%, #CB78EA 80%)',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '976px',
+        xl: '1024px',
+        '2xl': '1440px',
+      },
+      fontFamily: {
+        sansSerif: ['Pretendard', 'Noto Sans CJK KR', 'Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        sm: '0.675rem',
+        base: '1rem',
+        lg: '1.5rem',
+        xl: '2.25rem',
+      },
+      fontWeight: {
+        light: '300',
+        normal: '400',
+        medium: '500',
+        bold: '700',
+      },
+      height: {
+        sm: '2rem',
+        md: '3rem',
+        lg: '3.75rem', // 60px
+        xl: '6.25rem', // 100px
+        '2xl': '15.625rem', // 250px
+      },
+      spacing: {
+        sm: '0.5rem',
+        md: '1rem',
+        lg: '1.5rem',
+        xl: '2rem',
+        '2xl': '4rem',
+      },
+      borderRadius: {
+        sm: '8px',
+        md: '16px',
+        lg: '24px',
+        xl: '32px',
+      },
+      boxShadow: {
+        light: '0 4px 4px rgba(0, 0, 0, 0.10)',
+        heavy: '0 4px 4px rgba(0, 0, 0, 0.25)',
+      },
+      zIndex: {
+        60: '60',
+        70: '70',
+        80: '80',
+        100: '100',
       },
     },
   },
