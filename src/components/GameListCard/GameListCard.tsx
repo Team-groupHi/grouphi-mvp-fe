c8c45677784f05ecc243d883d615329ad5debd1c
Suyoung Shin <joyswim.dev@gmail.com>
2024-10-19 00:30:38 +0900

    ✨feat: flexBasis, maxWidth 카드 사이즈 조절 기능 추가

--- src/components/GameListCard/GameListCard.tsx
+++ src/components/GameListCard/GameListCard.tsx
@@ -1,13 +1,24 @@
 interface gameCardProps {
   title: string
   description?: string
+  flexBasis?: string | number
+  maxWidth?: string
 }
 
-const GameListCard = ({ title, description }: gameCardProps) => {
+const GameListCard = ({
+  title,
+  description,
+  flexBasis = '1/3',
+  maxWidth = 'xs',
+  ...props
+}: gameCardProps) => {
   return (
-    <article>
-      <p>{title}</p>
-      <p>{description}</p>
+    <article
+      className={`basis-${flexBasis} max-w-${maxWidth} p-md bg-primary-container p-sm shadow rounded-md`}
+      {...props}
+    >
+      <p className="text font-medium">{title}</p>
+      {description && <p className="text-sm text-gray pt-2">{description}</p>}
     </article>
   )
 }
