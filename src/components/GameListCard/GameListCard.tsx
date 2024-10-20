0a8ab119bb3c202209fbc3837040d5f76d457b39
Suyoung Shin <joyswim.dev@gmail.com>
2024-10-20 15:03:33 +0900

    💄design: 카드 bg-gradient, hover 수정

--- src/components/GameListCard/GameListCard.tsx
+++ src/components/GameListCard/GameListCard.tsx
@@ -31,7 +31,7 @@ const GameListCard = ({
         />
       )}
       <section
-        className={`relative place-content-center margin-auto h-full p-lg bg-black bg-opacity rounded-md z-10 hover:bg-opacity-high hover:cursor-pointer`}
+        className={`relative place-content-center margin-auto h-full p-lg bg-gradient-to-b from-transparent to-black/65 to-50% rounded-md z-10 hover:bg-black/30 hover:cursor-pointer`}
       >
         <p className="text font-medium truncate">{title}</p>
         {description && (
