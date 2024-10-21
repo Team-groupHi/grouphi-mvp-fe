6984a4d8c09a4283e28b70c78a3e115a57bd4af1
Suyoung Shin <joyswim.dev@gmail.com>
2024-10-21 19:06:16 +0900

    🎨format: prettier semi 설정 조정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
-'use client';
-import React from 'react';
+'use client'
+import React from 'react'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'solid' | 'secondary' | 'outline' | 'warn' | 'ghost' | 'link';
-  size?: 'sm' | 'md' | 'lg' | 'xl';
-  shape?: 'square' | 'full';
-  weight?: 'light' | 'normal' | 'bold';
-  isDisabled?: boolean;
-  children: string;
-  onClick: () => void;
+  variant?: 'solid' | 'secondary' | 'outline' | 'warn' | 'ghost' | 'link'
+  size?: 'sm' | 'md' | 'lg' | 'xl'
+  shape?: 'square' | 'full'
+  weight?: 'light' | 'normal' | 'bold'
+  isDisabled?: boolean
+  children: string
+  onClick: () => void
 }
 
 const Button = ({
@@ -20,7 +20,7 @@ const Button = ({
   onClick,
   ...props
 }: ButtonProps) => {
-  const upperCaseText = children.toUpperCase();
+  const upperCaseText = children.toUpperCase()
   const variantsClass = {
     solid: 'bg-primary text-dark',
     secondary: 'bg-primary-light text-dark',
@@ -28,25 +28,25 @@ const Button = ({
     warn: 'bg-error text',
     ghost: 'text hover:bg-black/10',
     link: 'text hover:bg-black/10 underline underline-offset-4',
-  };
+  }
   const disabledClass = isDisabled
     ? 'grayscale cursor-default text-gray-700'
-    : 'hover:bg-opacity-85';
+    : 'hover:bg-opacity-85'
   const shapeClass = {
     square: 'rounded-md',
     full: 'rounded-full',
-  };
+  }
   const sizeClass = {
     sm: 'px-md py-xs text-sm',
     md: 'px-lg py-sm text-md',
     lg: 'px-xl py-sm text-lg',
     xl: 'px-2xl py-sm text-xl',
-  };
+  }
   const weightClass: { [key: string]: string } = {
     light: 'font-light',
     normal: 'font-normal',
     bold: 'font-bold',
-  };
+  }
 
   return (
     <button
@@ -57,7 +57,7 @@ const Button = ({
     >
       {upperCaseText}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
