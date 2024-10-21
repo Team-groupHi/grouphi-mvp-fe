0b839b0a1e26ab46bd259cc3c4c8e42b0213df7e
Suyoung Shin <joyswim.dev@gmail.com>
2024-10-21 14:25:04 +0900

    🎨format: switch문 tailwind 동적 스타일로 변경

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,56 +7,30 @@ interface ButtonProps {
   onClick: () => void
 }
 
-const Button = ({ variant = 'solid', children, onClick }: ButtonProps) => {
+const Button = ({
+  variant = 'solid',
+  children,
+  onClick,
+  ...props
+}: ButtonProps) => {
   const upperCaseText = children.toUpperCase()
-  switch (variant) {
-    case 'secondary':
-      return (
-        <button
-          className="px-lg py-sm bg-primary-light rounded-full hover:opacity-85"
-          onClick={onClick}
-        >
-          {upperCaseText}
-        </button>
-      )
-    case 'outline':
-      return (
-        <button
-          className="px-lg py-sm border border-gray rounded-full text hover:opacity-80"
-          onClick={onClick}
-        >
-          {upperCaseText}
-        </button>
-      )
-    case 'ghost':
-      return (
-        <button
-          className="px-lg py-sm rounded-full text hover:bg-black/10"
-          onClick={onClick}
-        >
-          {upperCaseText}
-        </button>
-      )
-    case 'warn':
-      return (
-        <button
-          className="px-lg py-sm bg-error rounded-full text hover:opacity-85"
-          onClick={onClick}
-        >
-          {upperCaseText}
-        </button>
-      )
-    case 'solid':
-    default:
-      return (
-        <button
-          className="px-lg py-sm bg-primary rounded-full hover:opacity-85"
-          onClick={onClick}
-        >
-          {upperCaseText}
-        </button>
-      )
+  const variantsClass = {
+    solid: 'bg-primary',
+    secondary: 'bg-primary-light',
+    outline: 'border border-gray text',
+    warn: 'bg-error text',
+    ghost: 'text hover:bg-black/10',
   }
+
+  return (
+    <button
+      className={`px-lg py-sm rounded-full ${variantsClass[variant]}`}
+      onClick={onClick}
+      {...props}
+    >
+      {upperCaseText}
+    </button>
+  )
 }
 
 export default Button
