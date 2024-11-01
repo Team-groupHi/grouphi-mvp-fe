546573a533ae96a66bc856d20c4a5777ac26fb06
Suyoung Shin <joyswim.dev@gmail.com>
2024-11-02 00:09:40 +0900

    💄design: 최소 높이(min-h-52) 추가

--- src/components/GameListCard/GameListCard.tsx
+++ src/components/GameListCard/GameListCard.tsx
@@ -18,7 +18,7 @@ const GameListCard = ({
   ...props
 }: gameListCardProps) => {
   const containerClassName =
-    'basis-1/3 max-w-80 min-w-60 relative bg-primary-container shadow rounded-md overflow-hidden';
+    'basis-1/3 max-w-80 min-w-60 min-h-52 relative bg-primary-container shadow rounded-md overflow-hidden';
   return (
     <article
       data-testid="gamelistcard-container"
