96bbcc7003da7c6a76c03c75aabcd8375fa07d59
SoJuSo <asdfg7123@gmail.com>
2024-11-04 20:21:22 +0900

    ✨feat: 바뀐 tailwind 설정에 맞게 코드 수정

--- src/components/BalanceGameQuestionCard/BalanceGameQuestionCard.tsx
+++ src/components/BalanceGameQuestionCard/BalanceGameQuestionCard.tsx
@@ -14,15 +14,15 @@ const BalanceGameQuestionCard = ({
 
   return (
     <section
-      className={`max-h-60 max-w-60 min-h-40 min-w-40 flex items-center justify-center border-2 rounded-lg cursor-pointer bg-primary-container ${
+      className={`max-h-60 max-w-60 min-h-40 min-w-40 flex items-center justify-center border-2 rounded-lg cursor-pointer bg-container ${
         isSelected
-          ? 'border-purple-500'
+          ? 'border-secondary-500'
           : 'border-transparent hover:border-gray-300'
       }`}
       onClick={onSelect}
       {...props}
     >
-      <span className="text-white font-bold text-lg">{label}</span>
+      <span className="text-light font-bold text-lg">{label}</span>
     </section>
   );
 };
