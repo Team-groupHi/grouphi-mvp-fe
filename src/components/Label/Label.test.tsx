e2d4673fd584bfd2ca8a987f10cd74d66cd3f170
SoJuSo <asdfg7123@gmail.com>
2024-11-06 19:36:52 +0000

    🔧chore: shadcn 설치

---
+++ src/components/Label/Label.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Label from './Label';
+
+describe('Label Component', () => {
+  it('1) font-size는 sm, font-weight는 medium의 텍스트가 잘 출력된다.', () => {
+    const { container, getByText } = render(<Label>Test Label</Label>);
+    expect(getByText('Test Label')).toBeInTheDocument();
+    expect(container.firstChild).toHaveClass('text-sm');
+    expect(container.firstChild).toHaveClass('font-medium');
+  });
+
+  it('2) 커스텀 class name을 부여할 수 있다.', () => {
+    const { container } = render(
+      <Label className="custom-class">Custom Class Label</Label>
+    );
+    expect(container.firstChild).toHaveClass('custom-class');
+  });
+});
