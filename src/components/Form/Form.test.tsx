db67e14919fa1ce3bc90849f3f0770647f27ae78
kim-hyunjoo <khj9x.x@gmail.com>
2024-11-07 04:40:17 +0900

    🧪test: 불필요한 주석 삭제

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -22,7 +22,6 @@ const MockForm = () => {
     }),
   });
 
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -30,10 +29,7 @@ const MockForm = () => {
     },
   });
 
-  // 2. Define a submit handler.
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   };
 
@@ -89,30 +85,27 @@ describe('Form 컴포넌트 테스트', () => {
   });
 
   it('4) Username 입력 필드에 유효하지 않은 값을 입력하고 제출하면 에러 메시지가 표시되며 onSubmit이 실행되지 않는다.', async () => {
-    console.log = vi.fn(); // console.log를 모킹
+    console.log = vi.fn();
 
     const input = screen.getByPlaceholderText('shadcn');
     fireEvent.change(input, { target: { value: 'a' } }); // 2자 미만 입력
     fireEvent.click(screen.getByRole('button', { name: /submit/i }));
 
-    // 에러 메시지가 표시되는지 확인
     expect(
       await screen.findByText('Username must be at least 2 characters.')
     ).toBeInTheDocument();
 
-    // onSubmit이 호출되지 않았는지 확인
     expect(console.log).not.toHaveBeenCalled();
   });
 
   it('5) Username 입력 필드에 유효한 값을 입력하고 제출하면 콘솔에 값이 출력된다.', async () => {
-    console.log = vi.fn(); // console.log를 모킹
+    console.log = vi.fn();
 
     const input = screen.getByPlaceholderText('shadcn');
     fireEvent.change(input, { target: { value: 'validUsername' } });
     fireEvent.click(screen.getByRole('button', { name: /submit/i }));
 
-    // 비동기 작업 대기
-    await screen.findByText('This is your public display name.'); // 해당 메시지가 렌더링될 때까지 대기
+    await screen.findByText('This is your public display name.');
 
     expect(console.log).toHaveBeenCalledWith({ username: 'validUsername' });
   });
