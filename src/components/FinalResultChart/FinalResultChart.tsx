7611a0bd251aeebc57a3f2ed6b83da774b7955b3
kim-hyunjoo <khj9x.x@gmail.com>
2024-11-12 02:15:50 +0900

    ✨feat: FinalResultChart 제목, 버튼 추가
    - 시맨틱 태그 수정

--- src/components/FinalResultChart/FinalResultChart.tsx
+++ src/components/FinalResultChart/FinalResultChart.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/Button';
 import Bar, { BarProps } from './Bar';
 
 interface FinalResultChartProps {
@@ -6,14 +7,24 @@ interface FinalResultChartProps {
 
 const FinalResultChart = ({ data }: FinalResultChartProps) => {
   return (
-    <div className="bg-container-600 flex flex-col gap-3">
-      {data.map((result, index) => (
-        <Bar
-          key={index}
-          {...result}
-        ></Bar>
-      ))}
-    </div>
+    <section className="bg-container-600 flex flex-col justify-center items-center rounded-lg gap-8 p-4">
+      <h1 className="text-xl font-semibold">최종 결과</h1>
+      <section className="w-full gap-3 flex flex-col justify-center items-center">
+        {data.map((result, index) => (
+          <Bar
+            key={index}
+            {...result}
+          ></Bar>
+        ))}
+      </section>
+      <Button
+        shape="square"
+        size={'xl'}
+        className="font-semibold"
+      >
+        결과 공유하기
+      </Button>
+    </section>
   );
 };
 
