2189e65b5955f837b351d959b77378c62c8bcc10
kim-hyunjoo <khj9x.x@gmail.com>
2024-11-12 03:46:33 +0900

    💄design: FinalResultChart height 80%로 변경

--- src/components/FinalResultChart/FinalResultChart.tsx
+++ src/components/FinalResultChart/FinalResultChart.tsx
@@ -7,7 +7,7 @@ interface FinalResultChartProps {
 
 const FinalResultChart = ({ data }: FinalResultChartProps) => {
   return (
-    <section className="bg-container-600 h-4/6 flex flex-col justify-center items-center rounded-lg gap-8 p-4">
+    <section className="bg-container-600 h-4/5 w-full flex flex-col justify-center items-center rounded-lg gap-8 p-4">
       <h1 className="text-xl font-semibold">최종 결과</h1>
       <section className="w-full h-3/5 overflow-y-auto gap-3 flex flex-col items-center">
         {data.map((result, index) => (
