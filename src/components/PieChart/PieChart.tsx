2259635240d6b767e2ef12c2da933bd365f9f34d
joyswim <joyswim.dev@gmail.com>
2024-11-18 14:34:52 +0900

    PieChart 컴포넌트 (#52)
    * 🔧chore: chart.js 설치
    * ✨feat: 기본 파이 차트 구현
    * 💄design: 차트 디자인 수정
    * 🧪test: PieChart 테스트 코드 작성
    * ✨feat: 데이터가 없는 경우 표시할 문구 설정

---
+++ src/components/PieChart/PieChart.tsx
@@ -0,0 +1,90 @@
+'use client';
+import React, { HTMLAttributes } from 'react';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { cn } from '@/lib/utils';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+interface PieChartProps extends HTMLAttributes<HTMLDivElement> {
+  labels: string[];
+  data: number[];
+  options?: ChartOptions<'doughnut'>;
+  className?: string;
+}
+
+const PieChart = ({
+  labels,
+  data,
+  options,
+  className,
+  ...props
+}: PieChartProps) => {
+  const defaultClassName = 'w-96';
+  const combinedClassName = className
+    ? cn(defaultClassName, className)
+    : defaultClassName;
+  const combinedData: ChartData<'doughnut'> = {
+    labels,
+    datasets: [
+      {
+        label: '총 합계: ',
+        data,
+        backgroundColor: ['#32e4d0', '#cb78ea', '#fff0b6'],
+        borderWidth: 0,
+        hoverBackgroundColor: '#93d6ce80',
+        hoverBorderWidth: 2,
+        hoverOffset: 10,
+      },
+    ],
+  };
+  const combinedOptions: ChartOptions<'doughnut'> = {
+    responsive: true,
+    layout: {
+      padding: {
+        bottom: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: '#F0F0F0',
+          font: {
+            size: 14,
+          },
+        },
+      },
+    },
+    ...options,
+  };
+
+  if (data.length === 0) {
+    return (
+      <section className="min-w-96 flex justify-center items-center rounded bg-black/50">
+        데이터가 없습니다
+      </section>
+    );
+  }
+
+  return (
+    <section
+      className={combinedClassName}
+      {...props}
+    >
+      <Doughnut
+        data-testid="pie-chart"
+        data={combinedData}
+        options={combinedOptions}
+      />
+    </section>
+  );
+};
+
+export default PieChart;
