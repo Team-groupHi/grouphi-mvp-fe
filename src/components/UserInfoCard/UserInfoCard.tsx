f5f75b12375a974856ae58642d2dff24ec838780
kim-hyunjoo <khj9x.x@gmail.com>
2024-11-25 07:18:18 +0900

    ✨feat: UserInfoCard 컴포넌트 구현
    - 이미지는 로컬에서 테스트 이미지를 가져옴
    - 디자인 적용 완료

--- src/components/UserInfoCard/UserInfoCard.tsx
+++ src/components/UserInfoCard/UserInfoCard.tsx
@@ -1,3 +1,30 @@
-const UserInfoCard = () => {};
+import Image from 'next/image';
+//@TODO: 이미지 불러오는 방식 추후 결정
+import example from '@/app/assets/profile_test.png';
+
+interface UserInfoCardProps {
+  name: string;
+  isReady: boolean;
+}
+
+const UserInfoCard = ({ name, isReady }: UserInfoCardProps) => {
+  return (
+    <section
+      className={`${isReady ? 'bg-primary/50' : 'bg-container'} w-[250px] h-[65px] flex rounded-lg`}
+    >
+      <figure className="rounded-l-lg overflow-hidden">
+        <Image
+          src={example}
+          alt="profile"
+          width={65}
+          height={65}
+        ></Image>
+      </figure>
+      <div className="px-5 flex items-center font-bold">
+        <span>{name}</span>
+      </div>
+    </section>
+  );
+};
 
 export default UserInfoCard;
