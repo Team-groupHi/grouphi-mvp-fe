aca9907112ec8825f8c3fbe7e0799272eac7ff45
kim-hyunjoo <khj9x.x@gmail.com>
2024-12-04 05:29:29 +0900

    💄design: Bar 차트 height 고정 길이 삭제

--- src/components/FinalResultChart/FinalResultChart.tsx
+++ src/components/FinalResultChart/FinalResultChart.tsx
@@ -9,7 +9,7 @@ const FinalResultChart = ({ data }: FinalResultChartProps) => {
   return (
     <section className="bg-container-600 h-full w-full flex flex-col justify-center items-center rounded-lg gap-8 p-8">
       <h1 className="text-lg font-semibold">최종 결과</h1>
-      <section className="w-full h-4/5 overflow-y-auto gap-2 flex flex-col items-center">
+      <section className="w-full overflow-y-auto gap-2 flex flex-col items-center">
         {data.map((result, index) => (
           <Bar
             key={index}
