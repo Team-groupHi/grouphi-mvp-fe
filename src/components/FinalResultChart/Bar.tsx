dda4b16eae14c1ad71234a9da48d21374040f059
kim-hyunjoo <khj9x.x@gmail.com>
2024-12-03 20:58:22 +0900

    ✨feat: total 0인 경우 회색 배경 처리 및 코드 리팩토링
    - Bar의 겹치는 코드는 BarItem으로 통합

--- src/components/FinalResultChart/Bar.tsx
+++ src/components/FinalResultChart/Bar.tsx
@@ -4,12 +4,13 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/Tooltip';
+import clsx from 'clsx';
 
-export interface BarProps {
-  candidate1: string;
-  votes1: number;
-  candidate2: string;
-  votes2: number;
+interface BarItemProps {
+  className: string;
+  isLeft: boolean;
+  percentage: number;
+  votes: number;
 }
 
 const Bar = ({ votes1, candidate1, votes2, candidate2 }: BarProps) => {
@@ -22,39 +23,62 @@ const Bar = ({ votes1, candidate1, votes2, candidate2 }: BarProps) => {
     <div className="w-full flex items-center justify-center gap-6">
       <span className="min-w-[20%] text-right">{candidate1}</span>
 
-      <div className="flex h-8 min-w-[50%]">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger
-              className={`bg-primary ${votes2 === 0 && 'rounded-r-full'} rounded-l-full flex items-center justify-center`}
-              style={{ width: `${percentageCandidate1}%` }}
-            >
-              <span className="font-bold bg-clip-text text-transparent bg-gradient-purple text-center w-full">{`${votes1}`}</span>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>{percentageCandidate1.toFixed(1)}%</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+      {totalVotes === 0 ? (
+        <div className="min-w-[50%] p-1 bg-container-100 rounded-full flex items-center justify-center">
+          <span className="font-bold bg-clip-text text-transparent bg-gradient-purple text-center w-full">
+            0
+          </span>
+        </div>
+      ) : (
+        <div className="flex h-8 min-w-[50%]">
+          <BarItem
+            className={`bg-primary ${votes2 === 0 && 'rounded-r-full'}`}
+            isLeft={true}
+            percentage={percentageCandidate1}
+            votes={votes1}
+          />
 
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger
-              className={`bg-secondary ${votes1 === 0 && 'rounded-l-full'} rounded-r-full flex items-center justify-center`}
-              style={{ width: `${percentageCandidate2}%` }}
-            >
-              <span className="font-bold bg-clip-text text-transparent bg-gradient-purple text-center w-full">{`${votes2}`}</span>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p>{percentageCandidate2.toFixed(1)}%</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
+          <BarItem
+            className={`bg-secondary ${votes1 === 0 && 'rounded-l-full'} `}
+            isLeft={false}
+            percentage={percentageCandidate2}
+            votes={votes2}
+          />
+        </div>
+      )}
 
       <span className="min-w-[20%]">{candidate2}</span>
     </div>
   );
 };
+
+const BarItem = ({ className, isLeft, percentage, votes }: BarItemProps) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger
+        className={clsx(
+          className,
+          'flex items-center justify-center',
+          isLeft ? 'rounded-l-full' : 'rounded-r-full'
+        )}
+        style={{ width: `${percentage}%` }}
+      >
+        <span className="font-bold bg-clip-text text-transparent bg-gradient-purple text-center w-full">
+          {votes}
+        </span>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{`${percentage.toFixed(1)}%`}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
+
+export interface BarProps {
+  candidate1: string;
+  votes1: number;
+  candidate2: string;
+  votes2: number;
+}
 
 export default Bar;
