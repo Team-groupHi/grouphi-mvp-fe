cbad30e39dea9cbab11d89f77baef95c674c5225
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-10 23:30:10 +0900

    ✨feat: API 서버 요청으로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
-'use client';
 import { getGames } from '@/services/games';
 import { HomeClient } from '@/components';
-import { useEffect, useState } from 'react';
-import { GamesResponse } from '@/types/api';
 
-export default function Home() {
-  // const games = await getGames();
-
-  const [games, setGames] = useState<GamesResponse[]>([]);
-
-  const loadGames = async () => {
-    try {
-      const data = await getGames();
-      if (data) {
-        setGames(data);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  useEffect(() => {
-    loadGames();
-  }, []);
+export default async function Home() {
+  const games = await getGames();
 
   return <HomeClient games={games ?? []} />;
 }
