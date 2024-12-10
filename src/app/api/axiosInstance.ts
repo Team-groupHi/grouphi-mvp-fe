df180c5958fe41f7783a012869a3ba0c8d16a34a
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-10 23:24:27 +0900

    🎨format: 환경변수 수정에 따라 변수명 변경

--- src/app/api/axiosInstance.ts
+++ src/app/api/axiosInstance.ts
@@ -2,6 +2,6 @@ import { NETWORK } from '@/constants/api';
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_KEY,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: NETWORK.TIMEOUT,
 });
