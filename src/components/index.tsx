bb000a3e79c78aeb961aa7ca70fd5374ff17ad3c
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-12 15:54:23 +0900

    🎨format: 컴포넌트 import문 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,14 @@
 export { Button, buttonVariants } from './Button';
+export {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from './Carousel';
+export { GameListCard } from './GameListCard';
 export { Navigation } from './Navigation';
+export { HomeClient } from './HomeClient';
+export { default as Label } from './Label';
+export { default as Input } from './Input';
+export { ModalRenderer, ModalShell } from './Modals';
