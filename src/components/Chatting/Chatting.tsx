a601ccff18562bc6ea8ebe62af0119f60ecd083a
SoJuSo <asdfg7123@gmail.com>
2024-12-21 17:12:37 +0000

    ✨feat: 채팅 컴포넌트 더미 데이터로 기본 틀 잡기

---
+++ src/components/Chatting/Chatting.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import Input from '@/components/Input';
+import Item from './Item';
+import React, { useRef, useState } from 'react';
+
+interface Message {
+  name: string;
+  message: string;
+}
+
+interface ChattingProps {
+  messages: Message[];
+  myName: string;
+}
+
+const Chatting = ({ messages, myName }: ChattingProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const [isComposing, setIsComposing] = useState(false);
+
+  const handleSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (inputRef.current && !isComposing) {
+        console.log('Submitted:', inputRef.current.value);
+        inputRef.current.value = '';
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  };
+
+  return (
+    <section className="h-4/5">
+      <section className="bg-container-600 rounded-t-lg h-4/5 overflow-auto">
+        {messages.map((item, index) => (
+          <Item
+            key={index}
+            {...item}
+            index={index}
+            type={
+              item.name === 'system'
+                ? 'system'
+                : item.name === myName
+                  ? 'me'
+                  : 'others'
+            }
+          ></Item>
+        ))}
+        <div ref={messagesEndRef}></div>
+      </section>
+      <section className="bg-container-600 p-3 rounded-b-lg border-solid border-t-1 border-container-400">
+        <Input
+          ref={inputRef}
+          className="bg-container-700 border-transparent"
+          placeholder="엔터 키를 눌러 채팅 전송"
+          onKeyDown={handleSubmit}
+          onCompositionStart={() => setIsComposing(true)}
+          onCompositionEnd={() => setIsComposing(false)}
+        ></Input>
+      </section>
+    </section>
+  );
+};
+
+export default Chatting;
