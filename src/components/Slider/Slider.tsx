b959cae831d10676effe48cce1312720caf76b52
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-22 22:28:04 +0900

    💄design: Thumb cursor-pointer 추가

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -42,7 +42,7 @@ const Slider = React.forwardRef<
         <SliderPrimitive.Range className="absolute h-full bg-secondary-500" />
       </SliderPrimitive.Track>
       {scales}
-      <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-secondary-500 bg-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+      <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-secondary-500 bg-white ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer" />
     </SliderPrimitive.Root>
   );
 });
