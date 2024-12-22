f1a1a5b6fb7cfe391621c667ed266c3124eafdc8
kim-hyunjoo <khj9x.x@gmail.com>
2024-12-22 23:08:30 +0900

    💄design: 타이머 디자인 적용
    - 알람 아이콘 추가
    - primary 색으로 변경
    - 기타 크기 설정 적용

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AlarmClock } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
 
 interface TimerProps {
@@ -37,16 +38,17 @@ const Timer = ({ startTime, endTime }: TimerProps) => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center gap-2 w-full">
-      <span className="text-lg font-bold">{second}</span>
+    <section className="flex items-center justify-center gap-2 w-full text-title1 font-semibold">
+      <AlarmClock />
+      <span className="w-6 text-center">{second}</span>
 
-      <div className="relative w-full bg-gray-200 rounded h-6">
+      <div className="relative w-full bg-white/20 rounded h-4">
         <div
-          className="bg-blue-500 h-full rounded"
+          className="bg-primary h-full rounded"
           style={{ width: `${percentage}%` }}
         />
       </div>
-    </div>
+    </section>
   );
 };
 
