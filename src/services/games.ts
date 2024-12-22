1926d21a1bfcbc5120862caebb8f2e191a12a533
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-22 21:17:54 +0900

    💡comment: 주석 삭제

--- src/services/games.ts
+++ src/services/games.ts
@@ -3,9 +3,6 @@ import { axiosInstance } from '@/app/api/axiosInstance';
 import { DOMAIN } from '@/constants/api';
 import { GamesResponse } from '@/types/api';
 
-/**
- * 게임 목록 조회
- */
 export const getGames = async () => {
   try {
     const response = await axiosInstance.get<GamesResponse[]>(DOMAIN.GAMES);
