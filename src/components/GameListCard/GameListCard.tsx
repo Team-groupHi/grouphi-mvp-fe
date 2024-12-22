d78cf44725b36cf0e7a115da7bd5c8ddf8f052e1
SoJuSo <asdfg7123@gmail.com>
2024-12-22 06:16:07 +0000

    gameListCard 디자인 수정 (#77)
    * ✨feat: hover 시 생성하기 버튼 추가
    
    * 💡comment: spacing 주석 수정

--- src/components/GameListCard/GameListCard.tsx
+++ src/components/GameListCard/GameListCard.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '@/components';
+import { Plus } from 'lucide-react';
 
 interface gameListCardProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -17,6 +19,8 @@ const GameListCard = ({
   className,
   ...props
 }: gameListCardProps) => {
+  const [isHover, setIsHover] = useState(false);
+
   const containerClassName =
     'basis-1/3 max-w-80 min-w-60 min-h-52 relative bg-primary-container shadow rounded-md overflow-hidden';
   return (
@@ -38,14 +42,23 @@ const GameListCard = ({
       )}
       <section
         className={`relative place-content-center margin-auto h-full p-600 bg-gradient-transparent-black rounded-md z-10 hover:bg-black/30 hover:cursor-pointer`}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
       >
-        <p className="text font-semibold truncate">{title}</p>
-        {description && (
-          <p
-            className={`font-light text-sm text-gray-300 pt-2 overflow-hidden text-ellipsis line-clamp-2 break-all`}
-          >
-            {description}
-          </p>
+        <p className="text font-semibold truncate pb-400">{title}</p>
+        {isHover ? (
+          <Button className="mt-50 hover:bg-primary">
+            <Plus />
+            생성하기
+          </Button>
+        ) : (
+          description && (
+            <p
+              className={`font-light text-sm text-gray-300 overflow-hidden text-ellipsis line-clamp-2 break-all`}
+            >
+              {description}
+            </p>
+          )
         )}
       </section>
     </article>
