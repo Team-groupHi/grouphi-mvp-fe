bce6e4bde00dc8332e60e149fc2a51d2e6091dd5
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-23 16:59:00 +0900

    ✨feat: about 페이지 MainHeader 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import { MainHeader } from '@/components';
 import React from 'react';
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <main className="flex flex-col h-screen p-800">
+      <MainHeader />
+    </main>
+  );
 };
 
 export default AboutPage;
