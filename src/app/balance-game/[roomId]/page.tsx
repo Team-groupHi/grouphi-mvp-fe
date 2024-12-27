cbd487f46c1e94112f5b5f0c6c617c14eb04c739
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2024-12-27 21:05:12 +0900

    대기실 페이지 (#69)
    * ✨feat: WaitingRoomPage 경로 생성
    
    - /balance-game/[roomId] 경로에 페이지 생성
    
    * 💄design: Chatting height값 수정
    
    * ✨feat: 더미데이터 생성
    
    * ✨feat: WaitingRoom 레이아웃 디자인 설계
    
    * ✨feat: 주사위 사진 추가
    
    * ✨feat: 더미데이터 게임 정보 추가
    
    * ✨feat: 중앙 게임 대기 컴포넌트 구현
    
    * ✨feat: 초대 링크 복사 버튼 추가
    
    * ✨feat: waiting 버튼 variant 추가
    
    * 💄design: dark 모드 css 삭제
    
    * ✨feat: 버튼 디자인 대기, 시작 분리 구성
    
    * 💄design: 친구 초대 버튼 secondary로 변경
    
    * 💄design: 섹션 별 width 조정
    
    * 💄design: px -> rem 단위 변경
    
    * 💄design: 게임 section max width 70rem 수정
    
    * ✨feat: 메세지 수신 시 스크롤 맨 아래로 이동하는 기능 추가
    
    * ♻️refactor: isComposing 상태 제거
    
    - e.nativeEvent.isComposing 속성 활용하도록 변경
    
    * 🎨format: 사용하지 않는 import 삭제
    
    * ✨feat: 더미데이터 프로필 정보 업데이트
    
    * ✨feat: 방장/일반유저에 따른 버튼 디자인 변경
    
    - 내부의 더미데이터를 사용하여 임의로 설정했습니다.
    - 추후에 소켓 연결 후 로직 변경이 필요합니다.
    
    * ♻️refactor: filter -> reduce 로직 변경
    
    - 불필요한 연산 제거를 위함
    
    ---------
    
    Co-authored-by: Suyoung Shin <joyswim.dev@gmail.com>

---
+++ src/app/balance-game/[roomId]/page.tsx
@@ -0,0 +1,95 @@
+import Chatting from '@/components/Chatting';
+import { CHAT_DUMMY, MY_NAME, USER_DUMMY, GAME_INFO } from './DUMMY';
+import UserInfoCard from '@/components/UserInfoCard';
+import { GameListCard } from '@/components/GameListCard';
+import { Button } from '@/components/Button';
+import { Loader, Link, CheckCheck, MousePointer2 } from 'lucide-react';
+
+const WaitingRoom = () => {
+  // @TODO: 더미데이터를 활용한 로직이므로 추후에 소켓 연동 후 변경 필요
+  const readyCount = USER_DUMMY.reduce(
+    (count, { isReady }) => count + (isReady ? 1 : 0),
+    0
+  );
+  const isAllReady = readyCount === USER_DUMMY.length;
+  const isRoomManager = true;
+  const isReady = false;
+
+  return (
+    <section className="w-screen h-screen flex items-center justify-center px-10 gap-10 shrink-0">
+      <section className="flex flex-col gap-3 h-4/5 min-w-[15rem] max-w-[20rem] relative">
+        <Button
+          className="absolute -top-12 left-0"
+          size={'sm'}
+          variant={'secondary'}
+        >
+          <Link />
+          초대 링크 복사
+        </Button>
+        {USER_DUMMY.map((data, index) => (
+          <UserInfoCard
+            key={index}
+            {...data}
+          ></UserInfoCard>
+        ))}
+      </section>
+
+      <section className="h-4/5 min-w-[45rem] max-w-[70rem] w-full flex flex-col justify-center items-center bg-container/50 rounded-lg gap-7">
+        <span className="font-semibold">잠시 후 게임이 시작됩니다.</span>
+        <GameListCard
+          {...GAME_INFO}
+          className="h-16"
+        ></GameListCard>
+        <Button
+          className="text-base font-semibold"
+          size="xl"
+          variant={
+            isRoomManager
+              ? isAllReady
+                ? 'default'
+                : 'waiting'
+              : isReady
+                ? 'waiting'
+                : 'default'
+          }
+        >
+          {isRoomManager && isAllReady && (
+            <div className="flex items-center justify-center gap-2">
+              <CheckCheck />{' '}
+              <span>
+                게임 시작({readyCount}/{USER_DUMMY.length})
+              </span>{' '}
+            </div>
+          )}
+          {isRoomManager && !isAllReady && (
+            <div className="flex items-center justify-center gap-2">
+              <Loader />{' '}
+              <span>
+                준비 대기({readyCount}/{USER_DUMMY.length})
+              </span>{' '}
+            </div>
+          )}
+          {!isRoomManager && isReady && (
+            <div className="flex items-center justify-center gap-2">
+              <CheckCheck /> <span>준비 완료</span>
+            </div>
+          )}
+          {!isRoomManager && !isReady && (
+            <div className="flex items-center justify-center gap-2">
+              <MousePointer2 /> <span>준비 하기</span>
+            </div>
+          )}
+        </Button>
+      </section>
+
+      <section className="h-4/5 min-w-[15rem] max-w-[20rem]">
+        <Chatting
+          myName={MY_NAME}
+          messages={CHAT_DUMMY}
+        ></Chatting>
+      </section>
+    </section>
+  );
+};
+
+export default WaitingRoom;
