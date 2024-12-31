20635fe52293086a6d6ab3800af007f01220a2d1
Suyoung Shin <joyswim.dev@gmail.com>
2024-12-31 19:56:34 +0900

    ✨feat: 방장 닉네임 인풋 추가 및 RoomStore 연결

--- src/components/ModalList/CreateBalanceGameModal.tsx
+++ src/components/ModalList/CreateBalanceGameModal.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { useState } from 'react';
-import { Button, Label, ModalShell, Slider } from '@/components';
+import React from 'react';
+import { useRef, useState } from 'react';
+import { Button, Input, Label, ModalShell, Slider } from '@/components';
 import { createRoom } from '@/services/rooms';
 import { PATH } from '@/constants/router';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import useRoomStore from '@/store/useRoomStore';
 
 interface BalanceGameModalProps {
   closeModal: () => void;
@@ -21,48 +23,84 @@ const CreateBalanceGameModal = ({
     STEP: 2,
   };
 
-  const [questionCount, setQuestionCount] = useState([QUESTIONS_COUNT.MIN]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [count, setCount] = useState([QUESTIONS_COUNT.MIN]);
+
   const router = useRouter();
+  const { setRoomId, setHostName, setQuestionCount } = useRoomStore();
 
   const createRoomMutation = useMutation({
     mutationFn: () => createRoom(optionPropsNumber),
-    onSuccess: (data) => {
-      router.push(
-        `${PATH.BALANCE_GAME}/${data}?question-count=${questionCount}`
-      );
+    onSuccess: (roomId) => {
+      if (roomId) {
+        setRoomId(roomId);
+        router.push(`${PATH.BALANCE_GAME}/${roomId}`);
+      }
     },
   });
 
-  const handleCreateGame = () => {
-    closeModal();
-    createRoomMutation.mutate();
+  const handleInputEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
+  };
+
+  const handleCreateGame = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // todo: 유효성 검사
+    if (count[0] > 0 && inputRef.current?.value) {
+      setQuestionCount(count[0]);
+      setHostName(inputRef.current?.value);
+
+      closeModal();
+      createRoomMutation.mutate();
+    }
   };
 
   return (
     <ModalShell closeModal={closeModal}>
-      <section className="flex flex-col px-300 py-600 gap-700">
+      <form
+        id="create-room-form"
+        className="flex flex-col px-300 py-600 gap-700"
+        onSubmit={handleCreateGame}
+      >
         <section>
           <span className="font-bold">방 생성하기</span>
         </section>
-        <section className="flex gap-500 items-center text-end">
-          <Label
-            className="w-24"
-            htmlFor="question-count"
-          >
-            질문의 개수
-          </Label>
-          <Slider
-            name="question-count"
-            value={questionCount}
-            onValueChange={setQuestionCount}
-            min={QUESTIONS_COUNT.MIN}
-            max={QUESTIONS_COUNT.MAX}
-            step={QUESTIONS_COUNT.STEP}
-          />
-          <span className="w-4 text-subtitle">{questionCount}</span>
+        <section className="flex flex-col gap-600">
+          <section className="flex gap-500 items-center">
+            <Label
+              className="w-24"
+              htmlFor="question-count"
+            >
+              질문의 개수
+            </Label>
+            <Slider
+              name="question-count"
+              value={count}
+              onValueChange={setCount}
+              min={QUESTIONS_COUNT.MIN}
+              max={QUESTIONS_COUNT.MAX}
+              step={QUESTIONS_COUNT.STEP}
+            />
+            <span className="w-4 text-subtitle">{count}</span>
+          </section>
+          <section className="flex gap-500 items-center">
+            <Label
+              className="w-24"
+              htmlFor="nickname-input"
+            >
+              닉네임
+            </Label>
+            <Input
+              name="nickname-input"
+              onKeyDown={handleInputEnter}
+              ref={inputRef}
+            />
+          </section>
         </section>
         <section className="flex gap-300 self-end">
-          <Button onClick={handleCreateGame}>생성하기</Button>
+          <Button type="submit">생성하기</Button>
           <Button
             variant="secondary"
             onClick={closeModal}
@@ -70,7 +108,7 @@ const CreateBalanceGameModal = ({
             닫기
           </Button>
         </section>
-      </section>
+      </form>
     </ModalShell>
   );
 };
