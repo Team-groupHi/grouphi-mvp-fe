f494bfe67181658815adfa6060e824da1864e8c3
kim-hyunjoo <khj9x.x@gmail.com>
2025-01-03 00:03:38 +0900

    ✨feat: api type 현재 스웨거에 있는 모든 것 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -6,3 +6,30 @@ export interface GamesResponse {
   descriptionEn: 'string';
   thumbnailUrl: 'string' | null;
 }
+
+export interface RoomGetResponse {
+  id: string;
+  status: 'WAITING' | 'PLAYING';
+  game: GamesResponse;
+  hostName: string;
+  players: Player[];
+}
+
+export interface Player {
+  name: string;
+  isReady: boolean;
+}
+
+export interface BalanceGameResultGetResponse {
+  round: number;
+  q: string;
+  a: string;
+  b: string;
+  result: BalanceGameSelectionsResponse;
+}
+
+export interface BalanceGameSelectionsResponse {
+  a: string[];
+  b: string[];
+  c: string[];
+}
