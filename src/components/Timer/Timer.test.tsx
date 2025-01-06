f43a9a5641cfd0a7afa5bc7aaee5b357a75bbca4
kim-hyunjoo <khj9x.x@gmail.com>
2025-01-06 15:49:22 +0900

    🧪test: waitFor -> setTimeout 변경, 오차 범위 수정
    - 정확한 1초 테스트를 위해 setTimeout으로 변경
    - 100 -> 10으로 보정값 수정

--- src/components/Timer/Timer.test.tsx
+++ src/components/Timer/Timer.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import Timer from './Timer';
 
@@ -16,31 +16,27 @@ describe('Timer Component', () => {
     );
   });
   it('1) 타이머 종료 시간 이후에는 남은 시간이 0이 된다.', async () => {
-    await new Promise((resolve) => setTimeout(resolve, 3100)); // 살짝의 보정값 적용
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3010)); // 살짝의 보정값 적용
+    });
 
     expect(screen.getByText('0')).toBeInTheDocument();
   });
 
   it('2) 타이머를 3초로 설정하면 1초마다 남은 시간이 2, 1, 0으로 바뀐다.', async () => {
-    await waitFor(
-      () => {
-        expect(screen.getByText('2')).toBeInTheDocument();
-      },
-      { timeout: 2000 }
-    );
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1010)); // 살짝의 보정값 적용
+    });
+    expect(screen.getByText('2')).toBeInTheDocument();
 
-    await waitFor(
-      () => {
-        expect(screen.getByText('1')).toBeInTheDocument();
-      },
-      { timeout: 2000 }
-    );
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1010)); // 살짝의 보정값 적용
+    });
+    expect(screen.getByText('1')).toBeInTheDocument();
 
-    await waitFor(
-      () => {
-        expect(screen.getByText('0')).toBeInTheDocument();
-      },
-      { timeout: 2000 }
-    );
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1010)); // 살짝의 보정값 적용
+    });
+    expect(screen.getByText('0')).toBeInTheDocument();
   });
 });
