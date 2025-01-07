b2e4137b5295784760f004bf27ddd7ec44aa8d69
joyswim <joyswim.dev@gmail.com>
2025-01-07 21:16:05 +0900

    메인페이지(MainPage) 구현 (#83)
    * 💄design: 공통 layout 기본 padding 적용
    
    * ✨feat: Navigation 컴포넌트 추가
    
    * ✨feat: About 페이지 추가
    
    * ✨feat: 메인페이지 로고, 네비게이션 추가
    
    * ✨feat: gameList 레이아웃 작업
    
    * ✨feat: 게임리스트 Carousel 컴포넌트 교체
    
    * ✨feat: balance-game 페이지 생성
    
    * ✨feat: 게임 클릭 시 balancegame 페이지로 이동
    
    * ✨feat: CreateBalanceGameModal 생성
    
    * ✨feat: axios 인스턴스 생성
    
    * ✨feat: axiosErrorHandler 함수 추가
    
    * 🎨format: error 객체 타입 검사 얼리 리턴 추가
    
    * ✨feat: 게임 목록 조회 서비스 함수 추가
    
    * ✨feat: api 관련 상수, 타입 파일 추가
    
    * ♻️refactor: Home 클라이언트 로직 HomeClient로 분리
    
    * 🎨format: 환경변수 수정에 따라 변수명 변경
    
    * 🎨format: 타입스크립트 수정
    
    * ✨feat: API 서버 요청으로 수정
    
    * 🎨format: 컴포넌트 import문 추가
    
    * 🎨format: 더미데이터 삭제 및 GameListCard 필요한 데이터만 전달
    
    * ✨feat: 방 생성 이벤트 핸들러 연결 및 디자인 수정
    
    * 🔧chore: Tanstack Query 설치 및 설정
    
    * 🔧chore: TanstackQuery yarn.lock 파일 추가
    
    * ✨feat: 라우터 PATH 상수 추가
    
    * ✨feat: 방 생성 API 서비스 함수 추가
    
    * ✨feat: CreateBalanceGameModal 방 생성 API 추가
    
    * 🚚structure: 모달 파일 이름 변경에 따른 이동
    
    * ✨feat: CreateBalanceGameModal 모달에 gameId 전달
    
    * 💡comment: 주석 삭제
    
    * ✨feat: 질문 개수 선택 UI Input->Slider로 변경
    
    * ✨feat: Logo 컴포넌트 추가
    
    * ✨feat: 여러 페이지이면 슬라이드, 이전/다음 버튼 추가
    
    * ✨feat: MainHeader 컴포넌트 분리
    
    * ✨feat: about 페이지 MainHeader 추가
    
    * ✨feat: Logo 컴포넌트 확장 props 추가
    
    * ✨feat: API와 일치하도록 src null 타입 추가
    
    * ✨feat: API와 일치하도록 src null 타입 추가
    
    * 🔧chore: framer-motion 설치
    
    * ✨feat: 별똥별(ShootingStars) 배경 적용
    
    * ✨feat: RoomStore 추가
    
    * ✨feat: 방장 닉네임 인풋 추가 및 RoomStore 연결
    
    * ✨feat: 닉네임 공백일 경우 메세지 출력
    
    * 🎨format: gameId 타입 string으로 변경
    
    * 🎨format: 컴포넌트 배럴 익스포트에 Form 추가
    
    * ✨feat: form > Form 컴포넌트로 교체
    
    * 🎨format: Slider value 배열->값으로 변경
    
    ---------
    
    Co-authored-by: SeokJu Kim <asdfg7123@gmail.com>

---
+++ src/components/ModalList/CreateBalanceGameModal.tsx
@@ -0,0 +1,160 @@
+'use client';
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  Input,
+  Label,
+  ModalShell,
+  Slider,
+} from '@/components';
+import { FORM } from '@/constants/form';
+import { PATH } from '@/constants/router';
+import { createRoom } from '@/services/rooms';
+import useRoomStore from '@/store/useRoomStore';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+interface BalanceGameModalProps {
+  closeModal: () => void;
+  optionPropsNumber: number | string;
+}
+
+const CreateBalanceGameModal = ({
+  closeModal,
+  optionPropsNumber,
+}: BalanceGameModalProps) => {
+  const QUESTIONS_COUNT = {
+    MIN: 10,
+    MAX: 20,
+    STEP: 2,
+  };
+
+  const formSchema = z.object({
+    questionCount: z.number().min(QUESTIONS_COUNT.MIN).max(QUESTIONS_COUNT.MAX),
+    hostName: z
+      .string()
+      .trim()
+      .min(FORM.NAME.MIN, {
+        message: '닉네임은 2-15글자 이내 작성해주세요.(공백 제외)',
+      })
+      .max(FORM.NAME.MAX, {
+        message: '닉네임은 2-15글자 이내 작성해주세요.(공백 제외)',
+      }),
+  });
+
+  const router = useRouter();
+  const { setRoomId, setHostName, setQuestionCount } = useRoomStore();
+
+  const createRoomMutation = useMutation({
+    mutationFn: () => createRoom(optionPropsNumber.toString()),
+    onSuccess: (roomId) => {
+      if (roomId) {
+        setRoomId(roomId);
+        router.push(`${PATH.BALANCE_GAME}/${roomId}`);
+      }
+    },
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      hostName: '',
+      questionCount: QUESTIONS_COUNT.MIN,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log('form values : ', values);
+    setQuestionCount(values.questionCount);
+    setHostName(values.hostName);
+
+    createRoomMutation.mutate();
+    closeModal();
+  };
+
+  return (
+    <ModalShell closeModal={closeModal}>
+      <section>
+        <span className="font-bold">방 생성하기</span>
+      </section>
+      <Form {...form}>
+        <form
+          id="create-room-form"
+          className="flex flex-col px-300 py-600 gap-600"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="questionCount"
+            render={({ field: { value, onChange } }) => (
+              <FormItem>
+                <FormControl>
+                  <section className="flex gap-500 items-center min-h-9">
+                    <Label
+                      className="w-24"
+                      htmlFor="question-count"
+                    >
+                      질문의 개수
+                    </Label>
+                    <Slider
+                      defaultValue={[value]}
+                      onValueChange={(v) => onChange(v[0])}
+                      min={QUESTIONS_COUNT.MIN}
+                      max={QUESTIONS_COUNT.MAX}
+                      step={QUESTIONS_COUNT.STEP}
+                    />
+                    <span className="w-4 text-subtitle">{value}</span>
+                  </section>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="hostName"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <section className="flex gap-500 items-center">
+                    <Label
+                      className="w-24"
+                      htmlFor="nickname-input"
+                    >
+                      닉네임
+                    </Label>
+                    <section className="flex flex-col gap-200 w-full">
+                      <Input
+                        placeholder="2-15자 이내로 작성해주세요"
+                        {...field}
+                      />
+                    </section>
+                  </section>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <section className="flex gap-300 self-end">
+            <Button type="submit">생성하기</Button>
+            <Button
+              variant="secondary"
+              onClick={closeModal}
+            >
+              닫기
+            </Button>
+          </section>
+        </form>
+      </Form>
+    </ModalShell>
+  );
+};
+
+export default CreateBalanceGameModal;
