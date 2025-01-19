544c6c52a295b4b125b129a95fab698a411e7e45
SoJuSo <asdfg7123@gmail.com>
2025-01-19 22:55:01 +0900

    Co-authored-by: kim-hyunjoo <kim-hyunjoo@users.noreply.github.com> Co-authored-by: joyswim <joyswim@naver.com>

--- src/types/api.ts
+++ src/types/api.ts
@@ -34,3 +34,13 @@ export interface BalanceGameSelectionsResponse {
   b: string[];
   c: string[];
 }
+
+export interface BalanceGameRoundResponse {
+  totalRounds: number;
+  currentRound: number;
+  startTime: string;
+  endTime: string;
+  q: string;
+  a: string;
+  b: string;
+}
