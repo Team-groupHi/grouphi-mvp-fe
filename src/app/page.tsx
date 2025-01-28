1160cac89a66a88348500771a0f258e58ea55bec
SoJuSo <asdfg7123@gmail.com>
2025-01-28 22:47:14 +0900

    ✨feat: 별빛 배경을 전체 페이지에 적용하도록 수정
    Co-authored-by: kim-hyunjoo <kim-hyunjoo@users.noreply.github.com>
    Co-authored-by: joyswim <joyswim.dev@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import { getGames } from '@/services/games';
-import { HomeClient, ShootingStars, StarsBackground } from '@/components';
+import { HomeClient } from '@/components';
 
 export default async function Home() {
   const games = await getGames();
 
   return (
     <>
-      <StarsBackground />
-      <ShootingStars />
       <HomeClient games={games ?? []} />
     </>
   );
