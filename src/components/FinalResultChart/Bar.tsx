38cefd1e0a06370be5811bb2b9667fc9cebb9bd5
kim-hyunjoo <khj9x.x@gmail.com>
2025-01-30 08:04:42 +0900

    💄design: 최종 결과 차트 Bar 크기 일정하게 통일

--- src/components/FinalResultChart/Bar.tsx
+++ src/components/FinalResultChart/Bar.tsx
@@ -24,26 +24,30 @@ const Bar = ({ votes1, candidate1, votes2, candidate2 }: BarProps) => {
       <span className="w-[25%] text-right">{candidate1}</span>
 
       {totalVotes === 0 ? (
-        <div className="min-w-[50%] h-5 bg-container-100 rounded-full flex items-center justify-center">
+        <div className="w-[50%] h-5 bg-container-100 rounded-full flex items-center justify-center">
           <span className="font-bold text-sm text-purple text-center w-full">
             0
           </span>
         </div>
       ) : (
         <div className="flex h-5 w-[50%]">
-          <BarItem
-            className={clsx('bg-primary', votes2 === 0 && 'rounded-r-full')}
-            isLeft={true}
-            percentage={percentageCandidate1}
-            votes={votes1}
-          />
+          {votes1 !== 0 && (
+            <BarItem
+              className={clsx('bg-primary', votes2 === 0 && 'rounded-r-full')}
+              isLeft={true}
+              percentage={percentageCandidate1}
+              votes={votes1}
+            />
+          )}
 
-          <BarItem
-            className={clsx('bg-secondary', votes1 === 0 && 'rounded-l-full')}
-            isLeft={false}
-            percentage={percentageCandidate2}
-            votes={votes2}
-          />
+          {votes2 !== 0 && (
+            <BarItem
+              className={clsx('bg-secondary', votes1 === 0 && 'rounded-l-full')}
+              isLeft={false}
+              percentage={percentageCandidate2}
+              votes={votes2}
+            />
+          )}
         </div>
       )}
 
