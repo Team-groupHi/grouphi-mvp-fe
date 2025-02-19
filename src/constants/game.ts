7bc5ac4d1f4f7a6e533db0bf4c5108a4d219c9bd
Suyoung Shin <joyswim.dev@gmail.com>
2025-02-19 19:09:44 +0900

    ✨feat: 게임 영어 이름 상수 선언

---
+++ src/constants/game.ts
@@ -0,0 +1,6 @@
+export const GAME = {
+  COMPREHENSIVE_BALANCE_GAME: 'Comprehensive Balance Game',
+  CLASSIC_BALANCE_GAME: 'Classic Balance Game',
+  FOOD_BALANCE_GAME: 'Food Balance Game',
+  DATING_BALANCE_GAME: 'Dating Balance Game',
+};
