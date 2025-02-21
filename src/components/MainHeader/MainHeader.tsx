b472a6c37a2d9812003bbf10a217db1655ec93ee
Suyoung <joashy21@gmail.com>
2025-02-21 15:28:56 +0900

    🎨format: MainHeader 닉네임 표시 코드 NicknameBar 교체

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,18 +1,15 @@
 'use client';
 
 import React from 'react';
-import { Button, Logo, Navigation } from '@/components';
+import { Logo, Navigation } from '@/components';
 import { PATH } from '@/constants/router';
 import { usePathname, useRouter } from 'next/navigation';
-import { Edit, Loader } from 'lucide-react';
-import useModalStore from '@/store/useModalStore';
 import useRoomStore from '@/store/useRoomStore';
-import { MODAL_TYPE } from '@/constants/modal';
+import NicknameBar from './NicknameBar';
 
 const MainHeader = () => {
   const currentPath = usePathname();
   const router = useRouter();
-  const { openModal } = useModalStore();
   const { myName } = useRoomStore();
 
   const navigationItems = [
@@ -26,10 +23,6 @@ const MainHeader = () => {
     },
   ];
 
-  const handleNicknameEdit = () => {
-    openModal(MODAL_TYPE.CREATE_USERNAME);
-  };
-
   return (
     <section
       id="header"
@@ -37,24 +30,10 @@ const MainHeader = () => {
     >
       <Logo onClick={() => router.push(PATH.HOME)} />
       <section className="flex">
-        <section className="nickname bg-container-600 text-subtitle px-500 rounded-full flex justify-center items-center">
-          {myName ? (
-            <span className="pointer-events-none pr-1">
-              {`닉네임 : ${myName}`}
-            </span>
-          ) : (
-            <Loader className="w-5 h-5 animate-spin text-light" />
-          )}
-          {myName && (
-            <Button
-              variant="ghost"
-              className="px-2 h-8 rounded-full"
-              onClick={handleNicknameEdit}
-            >
-              <Edit />
-            </Button>
-          )}
-        </section>
+        <NicknameBar
+          nickname={myName}
+          isEdit={true}
+        />
         <Navigation
           items={navigationItems}
           disabled={currentPath}
