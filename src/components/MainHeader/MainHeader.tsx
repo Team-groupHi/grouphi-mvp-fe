7eb5926febbb7574c6b073682bc595dfbb802f59
Suyoung <joashy21@gmail.com>
2025-02-21 15:22:49 +0900

    🐛fix: UX 닉네임 깜빡임 이슈 해결->로컬스토리지에서 데이터 가져오는 동안 로딩 처리

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Button, Logo, Navigation } from '@/components';
 import { PATH } from '@/constants/router';
 import { usePathname, useRouter } from 'next/navigation';
-import { Edit } from 'lucide-react';
+import { Edit, Loader } from 'lucide-react';
 import useModalStore from '@/store/useModalStore';
 import useRoomStore from '@/store/useRoomStore';
 import { MODAL_TYPE } from '@/constants/modal';
@@ -37,17 +37,23 @@ const MainHeader = () => {
     >
       <Logo onClick={() => router.push(PATH.HOME)} />
       <section className="flex">
-        <section className="nickname bg-container-600 text-subtitle pl-500 px-2 rounded-full flex justify-center items-center">
-          <span className="pointer-events-none pr-1">
-            {`닉네임 : ${myName}`}
-          </span>
-          <Button
-            variant="ghost"
-            className="px-2 h-8 rounded-full"
-            onClick={handleNicknameEdit}
-          >
-            <Edit />
-          </Button>
+        <section className="nickname bg-container-600 text-subtitle px-500 rounded-full flex justify-center items-center">
+          {myName ? (
+            <span className="pointer-events-none pr-1">
+              {`닉네임 : ${myName}`}
+            </span>
+          ) : (
+            <Loader className="w-5 h-5 animate-spin text-light" />
+          )}
+          {myName && (
+            <Button
+              variant="ghost"
+              className="px-2 h-8 rounded-full"
+              onClick={handleNicknameEdit}
+            >
+              <Edit />
+            </Button>
+          )}
         </section>
         <Navigation
           items={navigationItems}
