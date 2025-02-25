3cbcec66a239be5d6aa01b16e4f507840ed85509
kim-hyunjoo <khj9x.x@gmail.com>
2025-02-25 14:24:23 +0900

    ♻️refactor: socket type 분리

--- src/types/api.ts
+++ src/types/api.ts
@@ -49,13 +49,3 @@ export interface BalanceGameSelectionsResponse {
   b: string[];
   c: string[];
 }
-
-export interface BalanceGameRoundResponse {
-  totalRounds: number;
-  currentRound: number;
-  startTime: string;
-  endTime: string;
-  q: string;
-  a: string;
-  b: string;
-}
