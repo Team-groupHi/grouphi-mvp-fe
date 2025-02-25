7cba960f2a0cf0b611bb86816ab295ccd989f325
kim-hyunjoo <khj9x.x@gmail.com>
2025-02-25 13:37:51 +0900

    ✨feat: games 상수 변경, 닉네임 변경 상수 추가

--- src/services/games.ts
+++ src/services/games.ts
@@ -5,7 +5,7 @@ import { GamesResponse } from '@/types/api';
 
 export const getGames = async () => {
   try {
-    const response = await axiosInstance.get<GamesResponse[]>(DOMAIN.GAMES);
+    const response = await axiosInstance.get<GamesResponse[]>(DOMAIN.GET_GAMES);
     return response.data;
   } catch (error) {
     axiosErrorHandler(error);
