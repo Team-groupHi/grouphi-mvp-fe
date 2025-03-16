d9ff145750b587da351bd6d007ee1356aef203c6
suyoung shin <joyswim.dev@gmail.com>
2025-03-16 19:26:27 +0900

    ✨feat: 앱 설명 메타데이터 변수 분리

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,12 +1,15 @@
 import { Metadata } from 'next/types';
 
 export const DOMAIN_URL = 'https://grouphi.kr';
+export const DEFAULT_DESCRIPTION =
+  '그루파이에서 아이스브레이킹 게임을 즐기고 추억을 남겨보세요!';
+
 export const METADATA: Metadata = {
   title: 'groupHi',
-  description: '그루파이에서 아이스브레이킹 게임을 즐기고 추억을 남겨보세요!',
+  description: DEFAULT_DESCRIPTION,
   openGraph: {
     title: 'groupHi',
-    description: '그루파이에서 아이스브레이킹 게임을 즐기고 추억을 남겨보세요!',
+    description: DEFAULT_DESCRIPTION,
     url: DOMAIN_URL,
     images: [
       {
@@ -20,7 +23,7 @@ export const METADATA: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'groupHi',
-    description: '그루파이에서 아이스브레이킹 게임을 즐기고 추억을 남겨보세요!',
+    description: DEFAULT_DESCRIPTION,
     images: '/images/twitter-image.png',
   },
 };
