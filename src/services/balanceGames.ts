fbee43f1bb46aea2658c541940e72caf282ce2fe
kim-hyunjoo <khj9x.x@gmail.com>
2025-03-18 16:04:12 +0900

    ✨feat: getBalanceGameResults try/catch문 삭제

--- src/services/balanceGames.ts
+++ src/services/balanceGames.ts
@@ -1,4 +1,3 @@
-import { axiosErrorHandler } from '@/app/api/axiosErrorHandler';
 import { axiosInstance } from '@/app/api/axiosInstance';
 import { DOMAIN } from '@/constants/api';
 import {
@@ -10,18 +9,15 @@ export const getBalanceGameResults = async ({
   roomId,
   round,
 }: BalanceGameResultRequest) => {
-  try {
-    const response = await axiosInstance.get<BalanceGameResultResponse[]>(
-      DOMAIN.GET_BALANCEGAME_RESULT,
-      {
-        params: {
-          roomId,
-          round,
-        },
-      }
-    );
-    return response.data;
-  } catch (error) {
-    axiosErrorHandler(error);
-  }
+  const response = await axiosInstance.get<BalanceGameResultResponse[]>(
+    DOMAIN.GET_BALANCEGAME_RESULT,
+    {
+      params: {
+        roomId,
+        round,
+      },
+    }
+  );
+
+  return response.data;
 };
