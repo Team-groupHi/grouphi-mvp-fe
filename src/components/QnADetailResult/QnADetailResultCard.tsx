a2e19e40665516a5f4930329a3faeb77201896b1
Suyoung Shin <joyswim.dev@gmail.com>
2025-04-15 17:17:57 +0900

    ✨feat: HeartIcon 기본 동작 기능 구현

--- src/components/QnADetailResult/QnADetailResultCard.tsx
+++ src/components/QnADetailResult/QnADetailResultCard.tsx
@@ -1,6 +1,10 @@
+'use client';
 import { HeartIcon } from 'lucide-react';
+import { useState } from 'react';
 
 import { Input } from '@/components';
+import { cn } from '@/lib/utils';
+import { theme } from '@/styles/theme';
 
 interface QnADetailResultCardProps {
   nickname: string;
@@ -11,18 +15,30 @@ const QnADetailResultCard = ({
   nickname,
   answer,
 }: QnADetailResultCardProps) => {
+  const [isLike, setIsLike] = useState(false);
+
+  const handleClickHeart = () => {
+    setIsLike(() => !isLike);
+  };
+
   return (
     <>
       <section className="flex gap-400">
-        <div className="flex justify-center items-center text-title3 nickname-card w-[15rem] bg-gradient-purple rounded-lg">
+        <div className="nickame-card flex justify-center items-center text-title3 nickname-card w-[15rem] bg-gradient-purple rounded-lg">
           {nickname}
         </div>
         <Input
           value={answer}
           readOnly
         />
-        <div>
-          <HeartIcon />
+        <div
+          className="heart-icon flex items-center cursor-pointer"
+          onClick={handleClickHeart}
+        >
+          <HeartIcon
+            className={cn(isLike ? 'text-secondary-500' : 'text-light')}
+            fill={isLike ? theme.colors.secondary[500] : 'none'}
+          />
         </div>
       </section>
     </>
