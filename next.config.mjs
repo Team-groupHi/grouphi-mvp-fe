6e4783025593c485f04998dbef8f3e49beb92a48
kim-hyunjoo <khj9x.x@gmail.com>
2025-04-18 13:58:14 +0900

    🔧chore: strictMode:false 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-};
+const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
