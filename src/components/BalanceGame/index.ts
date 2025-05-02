560360a90811eac4c4d7a4cb330d6362a3b3ac24
kim-hyunjoo <khj9x.x@gmail.com>
2025-05-02 16:16:13 +0900

    🚚structure: BalanceGameProgress -> BalanceGame 폴더명 변경
    - BalanceGameQuestionCard 위치 BalanceGame 폴더로 이동

---
+++ src/components/BalanceGame/index.ts
@@ -0,0 +1,3 @@
+export { default as BalanceGameControl } from './BalanceGameControl';
+export { default as BalanceGameProgress } from './BalanceGameProgress';
+export { default as BalanceGameQuestionCard } from './BalanceGameQuestionCard';
