7edec746384160d831e972f74bf78d5fa281c35a
kim-hyunjoo <khj9x.x@gmail.com>
2025-05-14 23:54:46 +0900

    🐛fix: qna 결과 조회 api 함수 url 백엔드에 맞춰 수정

--- src/services/qnaGames.ts
+++ src/services/qnaGames.ts
@@ -1,13 +1,13 @@
-import axios from 'axios';
-
+import { axiosInstance } from '@/app/api/axiosInstance';
+import { DOMAIN } from '@/constants/api';
 import { QnaGameResultGetResponse, QnaGameResultRequest } from '@/types/api';
 
 export const getQnaGameResults = async ({
   roomId,
   round,
 }: QnaGameResultRequest) => {
-  const response = await axios.get<QnaGameResultGetResponse[]>(
-    `https://api.grouphi.kr/qna-game/results`,
+  const response = await axiosInstance.get<QnaGameResultGetResponse[]>(
+    DOMAIN.GET_QNAGAME_RESULT,
     {
       params: {
         roomId,
