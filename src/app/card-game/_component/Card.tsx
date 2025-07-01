345a2dc9d1a080e084642b55d2b7ab9600ac5ee7
Suyoung Shin <joyswim.dev@gmail.com>
2025-07-01 16:12:15 +0900

    ✨feat: 카드 게임의 Card 컴포넌트 구현

---
+++ src/app/card-game/_component/Card.tsx
@@ -0,0 +1,47 @@
+'use client';
+import React, { useState } from 'react';
+
+import { cn } from '@/lib/utils';
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  initialValue?: boolean;
+}
+
+const Card = ({ initialValue = false, ...props }: CardProps) => {
+  const [flipped, setFlipped] = useState(initialValue);
+  const defaultFaceClass =
+    'absolute w-full h-full backface-hidden rounded-lg flex justify-center items-center text-xl text-white font-bold';
+
+  return (
+    <div className="perspective-1000 w-24 h-32">
+      <div
+        className={cn(
+          'relative rounded-xl w-full h-full transform transform-3d transition-transform hover:cursor-pointer',
+          !flipped ? 'rotate-y-180' : '',
+          props.className
+        )}
+        onClick={() => setFlipped(!flipped)}
+        {...props}
+      >
+        <div
+          className={cn(
+            defaultFaceClass,
+            'card-front bg-cardgame-a hover:bg-cardgame-a/90'
+          )}
+        >
+          A
+        </div>
+        <div
+          className={cn(
+            defaultFaceClass,
+            'card-back bg-cardgame-b rotate-y-180 hover:bg-cardgame-b/90'
+          )}
+        >
+          B
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
