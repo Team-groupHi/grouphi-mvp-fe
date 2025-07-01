8494dee148c59aea21f19429ec9b4653e559ca7c
Suyoung Shin <joyswim.dev@gmail.com>
2025-07-01 16:22:18 +0900

    ✨feat: width, height props 추가

--- src/app/card-game/_component/Card.tsx
+++ src/app/card-game/_component/Card.tsx
@@ -5,15 +5,25 @@ import { cn } from '@/lib/utils';
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   initialValue?: boolean;
+  width?: string | number;
+  height?: string | number;
 }
 
-const Card = ({ initialValue = false, ...props }: CardProps) => {
+const Card = ({
+  initialValue = false,
+  width = '6rem',
+  height = '8rem',
+  ...props
+}: CardProps) => {
   const [flipped, setFlipped] = useState(initialValue);
   const defaultFaceClass =
     'absolute w-full h-full backface-hidden rounded-lg flex justify-center items-center text-xl text-white font-bold';
 
   return (
-    <div className="perspective-1000 w-24 h-32">
+    <div
+      className="perspective-1000"
+      style={{ width, height }}
+    >
       <div
         className={cn(
           'relative rounded-xl w-full h-full transform transform-3d transition-transform hover:cursor-pointer',
