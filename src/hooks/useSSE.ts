cee1cc79f9db938f33c940f7eea6b8562a6fb0ed
kim-hyunjoo <khj9x.x@gmail.com>
2025-07-10 23:16:14 +0900

    ✨feat: useSSE 훅 구현

---
+++ src/hooks/useSSE.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+import { DOMAIN } from '@/constants/api';
+
+export const useSSE = () => {
+  const [time, setTime] = useState<string | null>(null);
+  const [error, setError] = useState<Event | null>(null);
+
+  useEffect(() => {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const getTimeUrl = `${baseUrl}${DOMAIN.GET_TIME}`;
+    const eventSource = new EventSource(getTimeUrl);
+
+    eventSource.addEventListener('time', (event) => {
+      const time = event.data.trim().replace(/^"|"$/g, '');
+      setTime(time);
+    });
+
+    eventSource.onerror = (event) => {
+      setError(event);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  return { time, error };
+};
