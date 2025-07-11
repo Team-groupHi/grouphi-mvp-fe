b7d438b8b172a3888c1ccbd0ba93525ad326efe9
kim-hyunjoo <khj9x.x@gmail.com>
2025-07-11 23:34:21 +0900

    ♻️refactor: deprecated 변수 아직 존재하므로 일단은 유지

--- src/types/websocket.ts
+++ src/types/websocket.ts
@@ -3,6 +3,8 @@
 export interface BalanceGameRoundResponse {
   totalRounds: number;
   currentRound: number;
+  startTime: string; // deprecated
+  endTime: string; // deprecated
   playSeconds: number;
   q: string;
   a: string;
