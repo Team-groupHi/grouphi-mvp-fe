b5e50b5d165299fdfe4d299574347e103760c314
kim-hyunjoo <khj9x.x@gmail.com>
2025-07-28 18:09:21 +0900

    ♻️refactor: baseUrl 유효하지 않은 경우 방어 코드 추가

--- src/hooks/useSSE.ts
+++ src/hooks/useSSE.ts
@@ -4,10 +4,14 @@ import { DOMAIN } from '@/constants/api';
 
 export const useSSE = () => {
   const [time, setTime] = useState<string | null>(null);
-  const [error, setError] = useState<Event | null>(null);
+  const [error, setError] = useState<Event | Error | null>(null);
 
   useEffect(() => {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    if (!baseUrl) {
+      setError(new Error('NEXT_PUBLIC_BASE_URL is not defined.'));
+      return;
+    }
     const getTimeUrl = `${baseUrl}${DOMAIN.GET_TIME}`;
     const eventSource = new EventSource(getTimeUrl);
 
