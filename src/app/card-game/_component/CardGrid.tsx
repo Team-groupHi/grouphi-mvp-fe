f5eef3ff54b0c4c2f4660bc90ec1502e8b40203b
Suyoung <joyswim.dev@gmail.com>
2025-07-29 20:19:12 +0900

    ♻️refactor: 한 번 뒤집힌 카드 안 뒤집도록 방어코드 추가

--- src/app/card-game/_component/CardGrid.tsx
+++ src/app/card-game/_component/CardGrid.tsx
@@ -27,6 +27,9 @@ const CardGrid = ({
 
   const handleCardClick = (index: number) => {
     setCardStates((prev) => {
+      if (prev[index] !== 0) {
+        return prev;
+      }
       const newStates = [...prev];
       newStates[index] = myTeam;
       return newStates;
