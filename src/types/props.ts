33984b9bfdcda000833cb2823de996314c816441
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-01 13:40:49 +0900

    ♻️refactor: GamePanelProps, GameContainerProps 공통 type으로 분리

---
+++ src/types/props.ts
@@ -0,0 +1,16 @@
+import * as StompJS from '@stomp/stompjs';
+
+import { Player, RoomResponse } from '@/types/api';
+
+export interface GamePanelProps {
+  game: string;
+  roomId: string;
+  roomDetail: RoomResponse;
+  players: Player[];
+  isRoomManager: boolean;
+  sendMessage: <T>(
+    params: Omit<StompJS.IPublishParams, 'body'> & { body?: T }
+  ) => void;
+}
+
+export type GameContainerProps = Omit<GamePanelProps, 'game'>;
