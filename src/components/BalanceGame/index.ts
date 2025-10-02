4e2fb9222fa74c5cc33dfafa5e4b651a6aae0662
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-02 23:28:29 +0900

    🚚structure: BalanceGameContainer->BalanceGamePanel 변경

--- src/components/BalanceGame/index.ts
+++ src/components/BalanceGame/index.ts
@@ -1,6 +1,6 @@
-export { BalanceGameContainer } from './BalanceGameContainer';
 export { BalanceGameControl } from './BalanceGameControl';
 export { BalanceGameFinalResult } from './BalanceGameFinalResult';
+export { BalanceGameContainer } from './BalanceGamePanel';
 export { BalanceGamePartialResult } from './BalanceGamePartialResult';
 export { BalanceGameProgress } from './BalanceGameProgress';
 export { BalanceGameQuestionCard } from './BalanceGameQuestionCard';
