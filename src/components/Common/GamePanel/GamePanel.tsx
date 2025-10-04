9de7286b01931f939db4160e2f0bde10a2f90819
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-05 00:19:14 +0900

    ✨feat: 아직 지원하지 않는 게임에 대한 방어 코드

--- src/components/Common/GamePanel/GamePanel.tsx
+++ src/components/Common/GamePanel/GamePanel.tsx
@@ -1,18 +1,33 @@
 'use client';
 
+import { useRouter } from 'next/navigation'; // 💡 useRouter 사용
+import { useEffect } from 'react';
+
 import { gameComponentMap } from '@/constants/gameComponentMap';
+import { PATH } from '@/constants/router';
+import { useToast } from '@/hooks/useToast';
 import { GamePanelProps } from '@/types/props';
 import { gameToType } from '@/utils/gameToType';
 
 const GamePanel = (props: GamePanelProps) => {
-  const gameType = gameToType(props.game);
-  const Component = gameComponentMap[gameType];
+  const router = useRouter();
+  const { toast } = useToast();
 
-  // @TODO: 추후에 Frontend Error Code로 관리
+  const gameType = gameToType(props.game);
+  const Component = gameType ? gameComponentMap[gameType] : null;
+
+  useEffect(() => {
+    if (!Component) {
+      toast({
+        variant: 'destructive',
+        title: `${props.game}은 지원하지 않는 게임 타입입니다.`,
+      });
+      router.push(PATH.HOME);
+    }
+  }, [Component, props.game, router, toast]);
+
   if (!Component) {
-    throw new Error(
-      `Invalid game type: No component found for game type "${gameType}" derived from game: "${props.game}"`
-    );
+    return null;
   }
 
   return <Component {...props} />;
