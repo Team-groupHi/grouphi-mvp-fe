cd09b5add637f37706b20d8b0d7de30291591b27
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-05 00:32:32 +0900

    ♻️refactor: switch문 삭제, 객체 조회 방식으로 변경
    - O(1) 성능의 안정성, 확장성을 위함

--- src/utils/gameToType.ts
+++ src/utils/gameToType.ts
@@ -1,16 +1,11 @@
-import { GAME_TYPES } from '@/constants/game';
-import { GAME } from '@/constants/game';
+import { GAME_TYPE_MAP } from '@/constants/gameTypeMap';
 
 export const gameToType = (game: string) => {
-  switch (game) {
-    case GAME.GAMES.COMPREHENSIVE_BALANCE_GAME:
-    case GAME.GAMES.CLASSIC_BALANCE_GAME:
-    case GAME.GAMES.FOOD_BALANCE_GAME:
-    case GAME.GAMES.DATING_BALANCE_GAME:
-      return GAME_TYPES.BALANCE;
-    case GAME.GAMES.QNA_GAME:
-      return GAME_TYPES.QNA;
-    default:
-      return null;
+  const gameType = GAME_TYPE_MAP[game];
+
+  if (!gameType) {
+    return null;
   }
+
+  return gameType;
 };
