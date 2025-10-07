635da7fa9b072e8d7f94b978a53c648eebd21aad
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-07 12:52:09 +0900

    ♻️refactor: 불필요한 함수 삭제

--- src/components/Common/TotalRoundsForm/TotalRoundsForm.tsx
+++ src/components/Common/TotalRoundsForm/TotalRoundsForm.tsx
@@ -14,7 +14,7 @@ import {
   Label,
   Slider,
 } from '@/components';
-import { GAME_QUESTIONS_COUNT, totalRoundsSchema } from '@/constants/form';
+import { GAME_QUESTIONS_COUNT } from '@/constants/form';
 import { GameType } from '@/types/game';
 
 interface TotalRoundsFormProps {
@@ -28,9 +28,8 @@ const TotalRoundsForm = ({
 }: TotalRoundsFormProps) => {
   const QUESTIONS_COUNT = GAME_QUESTIONS_COUNT[gameType];
 
-  const formSchema = totalRoundsSchema({
-    min: QUESTIONS_COUNT.MIN,
-    max: QUESTIONS_COUNT.MAX,
+  const formSchema = z.object({
+    totalRounds: z.number().min(QUESTIONS_COUNT.MIN).max(QUESTIONS_COUNT.MAX),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
--- src/constants/form.ts
+++ src/constants/form.ts
@@ -1,16 +1,9 @@
-import { z } from 'zod';
-
 export const FORM = {
   NAME: {
     MIN: 2,
     MAX: 15,
   },
 };
-
-export const totalRoundsSchema = ({ min, max }: { min: number; max: number }) =>
-  z.object({
-    totalRounds: z.number().min(min).max(max),
-  });
 
 export const GAME_QUESTIONS_COUNT = {
   QNA: {
