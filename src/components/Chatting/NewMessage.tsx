d68392441a161d42f428696d2cce93a4428cab72
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-10 17:56:01 +0900

    💄design: system 메세지의 경우 sender 생략

--- src/components/Chatting/NewMessage.tsx
+++ src/components/Chatting/NewMessage.tsx
@@ -9,12 +9,13 @@ interface NewMessageProps {
 const NewMessage = ({ message, sender, onClick }: NewMessageProps) => {
   return (
     <Button
-      className="justify-start w-full h-7 py-1 px-2 gap-1 rounded "
+      className="justify-start w-full h-7 py-1 px-2 gap-1 rounded"
       onClick={onClick}
-      variant={'secondary'}
+      variant="secondary"
     >
       <p className="text-body3 overflow-hidden text-ellipsis whitespace-nowrap">
-        {`${sender}: ${message}`}
+        {sender != 'system' && `${sender}: `}
+        {message}
       </p>
     </Button>
   );
