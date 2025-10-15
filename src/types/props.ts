2a1ded0f07752a43f149f3224192718b9d8555a7
kim-hyunjoo <khj9x.x@gmail.com>
2025-10-15 15:33:09 +0900

    ♻️refactor: 공통 type 분리

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,16 +1,24 @@
 import * as StompJS from '@stomp/stompjs';
-import { ComponentType } from 'react';
 
 import { RoomResponse } from '@/types/api';
 
-export interface GamePanelProps {
-  GamePanel: ComponentType<GamePanelProps>;
+import { GameType } from './game';
+
+export interface GameControllerProps {
   roomId: string;
   roomDetail: RoomResponse;
   isRoomManager: boolean;
   sendMessage: <T>(
     params: Omit<StompJS.IPublishParams, 'body'> & { body?: T }
   ) => void;
+  gameType: GameType;
 }
 
-export type GameControllerProps = Omit<GamePanelProps, 'GamePanel'>;
+export type PreGameControllerProps = Omit<GameControllerProps, 'roomId'>;
+
+export interface BalanceGameProgressProps {
+  sendMessage: <T>(
+    params: Omit<StompJS.IPublishParams, 'body'> & { body?: T }
+  ) => void;
+  setIsTimeout: (state: boolean) => void;
+}
