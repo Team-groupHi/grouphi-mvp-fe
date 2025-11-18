a3562545b14dc0386e67eba0e70369034a9bcc87
kim-hyunjoo <khj9x.x@gmail.com>
2025-11-18 20:22:09 +0900

    🎨format: 불필요한 toLowerCase 삭제

--- src/utils/deviceDetector.ts
+++ src/utils/deviceDetector.ts
@@ -2,5 +2,5 @@ export const isMobileDevice = (userAgent: string) => {
   const mobileRegex =
     /(android|iphone|ipod|ipad|windows phone|iemobile|mobile|tablet)/i;
 
-  return mobileRegex.test(userAgent.toLowerCase());
+  return mobileRegex.test(userAgent);
 };
